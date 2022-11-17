947b39c0adeb4c598f6155dc4249dc1e103b653e
Dani <danielalinodasilvaoliveira@gmail.com>
2022-11-17 16:52:01 -0300

    update

--- src/br/senai/sp/jandira/dao/EspecialidadeDao.java
+++ src/br/senai/sp/jandira/dao/EspecialidadeDao.java
@@ -3,6 +3,7 @@ package br.senai.sp.jandira.dao;
 import br.senai.sp.jandira.model.Especialidade;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -15,9 +16,11 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 
 public class EspecialidadeDao {
-    
-    private final static String URL ="C:\\Users\\22282183\\Java\\Especialidade.txt";
+
+    private final static String URL = "C:\\Users\\22282183\\Java\\Especialidade.txt";
+    private final static String URL_TEMP = "C:\\Users\\22282183\\Java\\Especialidade-temp.txt";
     private final static Path PATH = Paths.get(URL);
+    private final static Path PATH_TEMP = Paths.get(URL_TEMP);
 
     /*
     Essa classe será responsável pela persistência de dados
@@ -29,7 +32,7 @@ public class EspecialidadeDao {
 
     public static void gravar(Especialidade e) { // CREATE
         especialidades.add(e);
-        
+
         //***GRAVAR EM ARQUIVO ***
         try {
             BufferedWriter escritor;
@@ -43,7 +46,7 @@ public class EspecialidadeDao {
         } catch (IOException erro) {
             JOptionPane.showMessageDialog(null, "Ocorreu um erro");
         }
-        
+
     }
 
     public static ArrayList<Especialidade> getEspecialidades() { // READ
@@ -53,91 +56,131 @@ public class EspecialidadeDao {
     public static Especialidade getEspecialidade(Integer codigo) { // READ
 
         for (Especialidade e : especialidades) {
-            if (Objects.equals(e.getCodigo(), codigo)) {
+            if (e.getCodigo().equals(codigo)) {
                 return e;
             }
         }
-        
+
         return null;
-      
+
     }
-    
+
     public static void atualizar(Especialidade especialidadeAtualizada) { // UPDATE
         for (Especialidade e : especialidades) {
             if (Objects.equals(
                     e.getCodigo(),
                     especialidadeAtualizada.getCodigo())) {
-                        especialidades.set(
+                especialidades.set(
                         especialidades.indexOf(e),
                         especialidadeAtualizada);
                 break;
             }
         }
+        atualizarArquivo();
     }
-        
+
     public static void excluir(Integer codigo) { // DELETE
-
+        
         for (Especialidade e : especialidades) {
-            if (e.getCodigo() == codigo) {
-                especialidades.remove(e);
-                break;
-            }
-        }
-
+                if (e.getCodigo().equals(codigo)) {
+                    especialidades.remove(e);
+                    break;
+                } 
+                
+                }
+        atualizarArquivo();
+        
+       
     }
+    private static void atualizarArquivo(){
+        
+    //PASSO 01 - Criar uma representação dos arquivos que serão manipulados
+        File arquivoAtual = new File(URL);
+        File arquivoTemp = new File(URL_TEMP);
+
+        try {
+            //Criar o arquivo temporário
+            arquivoTemp.createNewFile();
+
+            //Abrir o arquivo temporáril para escrita
+            BufferedWriter bwTemp = Files.newBufferedWriter(
+                    PATH_TEMP,
+                    StandardOpenOption.APPEND,
+                    StandardOpenOption.WRITE);
+            
+            
+            //Iterar na lista para adicionar as especialidades no arquivo temporáril, 
+            //exceto o registro que  não queremos mais
+            for (Especialidade e : especialidades) {
+                bwTemp.write(e.getEspecialidadeSeparadaPorPontoEVirgula());
+                bwTemp.newLine();
+            }
+            
+            bwTemp.close();
+            
+            arquivoAtual.delete();
+            
+            arquivoTemp.renameTo(arquivoAtual);
+            
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     
+    }
+
     // Criar uma lista inicial de especialidades
     public static void criarListaEspecialidades() {
-        
+
         try {
             BufferedReader leitor = Files.newBufferedReader(PATH);
-            
-            String linha= leitor.readLine();
-            
-            while(linha != null) {
-                
+
+            String linha = leitor.readLine();
+
+            while (linha != null) {
+
                 // Transformar os dados da linha em uma especialidade
                 String[] vetor = linha.split(";");
                 Especialidade e = new Especialidade(
                         vetor[1],
                         vetor[2],
                         Integer.valueOf(vetor[0]));
-                
+
                 //Guardar Especialidade na lista
                 especialidades.add(e);
-                
+
                 //Ler a próxima linha
                 linha = leitor.readLine();
             }
             leitor.close();
-            
+
         } catch (IOException erro) {
             JOptionPane.showMessageDialog(
                     null,
                     "Ocorreu um erro ao ler o arquivo");
         }
-        
+
         System.out.println(especialidades.size());
-        
+
     }
-    
+
     public static DefaultTableModel getEspecialidadesModel() {
-        
+
         String[] titulos = {"CÓDIGO", "NOME DA ESPECIALIDADE", "DESCRIÇÃO"};
-        
+
         String[][] dados = new String[especialidades.size()][3];
         //o int tem que ser fora do for
-        
-        int i = 0; 
-        
-        for (Especialidade e : especialidades){
+
+        int i = 0;
+
+        for (Especialidade e : especialidades) {
             dados[i][0] = e.getCodigo().toString();
             dados[i][1] = e.getNome();
             dados[i][2] = e.getDescricao();
             i++;
         }
         DefaultTableModel model = new DefaultTableModel(dados, titulos);
-        
+
         return model;
     }
 
--- src/br/senai/sp/jandira/testes/TesteObjetos.java
+++ src/br/senai/sp/jandira/testes/TesteObjetos.java
@@ -11,150 +11,150 @@ import br.senai.sp.jandira.model.PlanoDeSaude;
 
 public class TesteObjetos {
 
-    public static void main(String[] args) {
-        
-        int[] a = {6, 9, 87, 35, 96};
-        int[] b = new int[4];
-        b[0] = 55;
-        b[1] = 55;
-        b[2] = 11;
-        b[3] = 99;
-        
-        int[][] c = {
-            {4, 6, 5},
-            {5, 9, 7},
-            {1, 13, 3},
-            {5, 5, 8}
-        };
-        
-        String[][] quitanda = {
-            {"banana","Maçã", "Uva", "Morango"},
-            {"Alface", "Couve", "Pepino", "Salsinha", "Pimentão", "Brócolis"},
-            {"Abacate", "Tomate", "Jiló", "Abóbora"}
-        };
-        
-        System.out.println(quitanda[1][4]);
-        System.out.println(c[2][1]);
-        
-        System.out.println("*******************************************************");
-
-        Especialidade e1 = new Especialidade("Cardiologia");
-        //e1.setNome("Cardiologia");
-        e1.setDescricao("Não deixa ter um ataque.");
-        
-        EspecialidadeDao.gravar(e1);
-
-        Especialidade e2 = new Especialidade();
-        e2.setNome("Gastroenterologia");
-        e2.setDescricao("Não deixa ficar com dor de barriga.");
-        
-        EspecialidadeDao.gravar(e2);
-
-        Especialidade e4 = new Especialidade("Otorrino", "Cuida do ouvido;");
-
-        EspecialidadeDao.gravar(e4);
-        
-        Especialidade e3 = new Especialidade();
-        e3.setNome("Qualquer coisa!");
-        
-        EspecialidadeDao.gravar(e3);
-
-        Especialidade e5 = new Especialidade();
-        e5.setNome("Especialidade 5");
-        
-        EspecialidadeDao.gravar(e5);
-        
-        for (Especialidade ee : EspecialidadeDao.getEspecialidades()) {
-            System.out.println(ee.getNome() + " - " + ee.getCodigo());
-        }
-        
-        EspecialidadeDao.excluir(101);
-        System.out.println("---------------------------------------");
-        for (Especialidade ee : EspecialidadeDao.getEspecialidades()) {
-            System.out.println(ee.getNome() + " - " + ee.getCodigo());
-        }
-        
-        System.out.println("-------------BUSCA-------------");
-        Especialidade procurada = EspecialidadeDao.getEspecialidade(102);
-        System.out.println(procurada.getNome());
-        
-        System.out.println("-----------UPDATE------------");
-        Especialidade especialidadeAtualizada = new Especialidade();
-        especialidadeAtualizada.setCodigo(104);
-        especialidadeAtualizada.setNome("Otorrinolaringologia");
-        especialidadeAtualizada.setDescricao("Essa é uma nova descrição");
-        EspecialidadeDao.atualizar(especialidadeAtualizada);
-        
-        System.out.println("------------------NOVO RESULTADO---------------------");
-        for (Especialidade ee : EspecialidadeDao.getEspecialidades()) {
-            System.out.println(ee.getNome() + " - " + ee.getCodigo());
-        }
-        
-        // Exibir a quantidade de especialidades criadas até agora
-        System.out.println("TOTAL DE ESPECIALIDADES -----> " + e1.getContador());
-        System.out.println(e1.getCodigo() + " - " + e1.getNome());
-        System.out.println(e2.getCodigo() + " - " + e2.getNome());
-        System.out.println(e3.getCodigo() + " - " + e3.getNome());
-        System.out.println(e4.getCodigo() + " - " + e4.getNome());
-        System.out.println(e5.getCodigo() + " - " + e5.getNome());
-
-        ArrayList<Especialidade> especialidades = new ArrayList<>();
-        especialidades.add(e1);
-        especialidades.add(e2);
-        especialidades.add(e3);
-        System.out.println(especialidades.size());
-
-        // Imprimir no console o nome das especialidades
-        // que estão armazenadas no arraylist
-        int i = 0;
-        while (i < especialidades.size()) {
-            System.out.println(especialidades.get(i).getNome());
-            i++;
-        }
-
-        // Utilização do for para iteração no arraylist
-//		for(contador; condição; acumulador) {
-//			
-//		}
-        System.out.println("------------------ FOR ------------------");
-        for (int x = 0; x < especialidades.size(); x++) {
-            System.out.println(especialidades.get(x).getNome());
-        }
-
-        // FOR EACH -> para cada
-        System.out.println("---------------- FOR EACH -----------------");
-        for (Especialidade e : especialidades) {
-            System.out.println(e.getNome());
-        }
-
-        // Criar 3 planos de saúde, armazenar em um arraylist
-        // e exibir o nome da operadora de cada um deles
-        // usando for each.
-//        PlanoDeSaude p1 = new PlanoDeSaude("Amil");
-//        PlanoDeSaude p2 = new PlanoDeSaude("Bradesco");
-//        PlanoDeSaude p3 = new PlanoDeSaude("Notredame");
+//    public static void main(String[] args) {
+//        
+//        int[] a = {6, 9, 87, 35, 96};
+//        int[] b = new int[4];
+//        b[0] = 55;
+//        b[1] = 55;
+//        b[2] = 11;
+//        b[3] = 99;
+//        
+//        int[][] c = {
+//            {4, 6, 5},
+//            {5, 9, 7},
+//            {1, 13, 3},
+//            {5, 5, 8}
+//        };
+//        
+//        String[][] quitanda = {
+//            {"banana","Maçã", "Uva", "Morango"},
+//            {"Alface", "Couve", "Pepino", "Salsinha", "Pimentão", "Brócolis"},
+//            {"Abacate", "Tomate", "Jiló", "Abóbora"}
+//        };
+//        
+//        System.out.println(quitanda[1][4]);
+//        System.out.println(c[2][1]);
+//        
+//        System.out.println("*******************************************************");
 //
-//        ArrayList<PlanoDeSaude> planos = new ArrayList<>();
-//        planos.add(p1);
-//        planos.add(p2);
-//        planos.add(p3);
+//        Especialidade e1 = new Especialidade("Cardiologia");
+//        //e1.setNome("Cardiologia");
+//        e1.setDescricao("Não deixa ter um ataque.");
+//        
+//        EspecialidadeDao.gravar(e1);
 //
-//        System.out.println("-------------- OPERADORA --------------");
-//        for (PlanoDeSaude o : planos) {
-//            System.out.println(o.getOperadora());
-//            System.out.println(o.getCodigo());
+//        Especialidade e2 = new Especialidade();
+//        e2.setNome("Gastroenterologia");
+//        e2.setDescricao("Não deixa ficar com dor de barriga.");
+//        
+//        EspecialidadeDao.gravar(e2);
+//
+//        Especialidade e4 = new Especialidade("Otorrino", "Cuida do ouvido;");
+//
+//        EspecialidadeDao.gravar(e4);
+//        
+//        Especialidade e3 = new Especialidade();
+//        e3.setNome("Qualquer coisa!");
+//        
+//        EspecialidadeDao.gravar(e3);
+//
+//        Especialidade e5 = new Especialidade();
+//        e5.setNome("Especialidade 5");
+//        
+//        EspecialidadeDao.gravar(e5);
+//        
+//        for (Especialidade ee : EspecialidadeDao.getEspecialidades()) {
+//            System.out.println(ee.getNome() + " - " + ee.getCodigo());
+//        }
+//        
+//        EspecialidadeDao.excluir(101);
+//        System.out.println("---------------------------------------");
+//        for (Especialidade ee : EspecialidadeDao.getEspecialidades()) {
+//            System.out.println(ee.getNome() + " - " + ee.getCodigo());
+//        }
+//        
+//        System.out.println("-------------BUSCA-------------");
+//        Especialidade procurada = EspecialidadeDao.getEspecialidade(102);
+//        System.out.println(procurada.getNome());
+//        
+//        System.out.println("-----------UPDATE------------");
+//        Especialidade especialidadeAtualizada = new Especialidade();
+//        especialidadeAtualizada.setCodigo(104);
+//        especialidadeAtualizada.setNome("Otorrinolaringologia");
+//        especialidadeAtualizada.setDescricao("Essa é uma nova descrição");
+//        EspecialidadeDao.atualizar(especialidadeAtualizada);
+//        
+//        System.out.println("------------------NOVO RESULTADO---------------------");
+//        for (Especialidade ee : EspecialidadeDao.getEspecialidades()) {
+//            System.out.println(ee.getNome() + " - " + ee.getCodigo());
+//        }
+//        
+//        // Exibir a quantidade de especialidades criadas até agora
+//        System.out.println("TOTAL DE ESPECIALIDADES -----> " + e1.getContador());
+//        System.out.println(e1.getCodigo() + " - " + e1.getNome());
+//        System.out.println(e2.getCodigo() + " - " + e2.getNome());
+//        System.out.println(e3.getCodigo() + " - " + e3.getNome());
+//        System.out.println(e4.getCodigo() + " - " + e4.getNome());
+//        System.out.println(e5.getCodigo() + " - " + e5.getNome());
+//
+//        ArrayList<Especialidade> especialidades = new ArrayList<>();
+//        especialidades.add(e1);
+//        especialidades.add(e2);
+//        especialidades.add(e3);
+//        System.out.println(especialidades.size());
+//
+//        // Imprimir no console o nome das especialidades
+//        // que estão armazenadas no arraylist
+//        int i = 0;
+//        while (i < especialidades.size()) {
+//            System.out.println(especialidades.get(i).getNome());
+//            i++;
 //        }
 //
-//        PlanoDeSaude xpto1 = new PlanoDeSaude();
-//        PlanoDeSaude xpto2 = new PlanoDeSaude();
-//        PlanoDeSaude xpto3 = new PlanoDeSaude();
+//        // Utilização do for para iteração no arraylist
+////		for(contador; condição; acumulador) {
+////			
+////		}
+//        System.out.println("------------------ FOR ------------------");
+//        for (int x = 0; x < especialidades.size(); x++) {
+//            System.out.println(especialidades.get(x).getNome());
+//        }
 //
-//        System.out.println("-------- " + PlanoDeSaude.getCodigo());
+//        // FOR EACH -> para cada
+//        System.out.println("---------------- FOR EACH -----------------");
+//        for (Especialidade e : especialidades) {
+//            System.out.println(e.getNome());
+//        }
 //
-//        AgendaApp.main(args);
+//        // Criar 3 planos de saúde, armazenar em um arraylist
+//        // e exibir o nome da operadora de cada um deles
+//        // usando for each.
+////        PlanoDeSaude p1 = new PlanoDeSaude("Amil");
+////        PlanoDeSaude p2 = new PlanoDeSaude("Bradesco");
+////        PlanoDeSaude p3 = new PlanoDeSaude("Notredame");
+////
+////        ArrayList<PlanoDeSaude> planos = new ArrayList<>();
+////        planos.add(p1);
+////        planos.add(p2);
+////        planos.add(p3);
+////
+////        System.out.println("-------------- OPERADORA --------------");
+////        for (PlanoDeSaude o : planos) {
+////            System.out.println(o.getOperadora());
+////            System.out.println(o.getCodigo());
+////        }
+////
+////        PlanoDeSaude xpto1 = new PlanoDeSaude();
+////        PlanoDeSaude xpto2 = new PlanoDeSaude();
+////        PlanoDeSaude xpto3 = new PlanoDeSaude();
+////
+////        System.out.println("-------- " + PlanoDeSaude.getCodigo());
+////
+////        AgendaApp.main(args);
+////
+////        System.out.println("-------- " + PlanoDeSaude.getCodigo());
 //
-//        System.out.println("-------- " + PlanoDeSaude.getCodigo());
-
-    }
+//    }
 
 }
